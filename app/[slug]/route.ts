5669e74ff91f8b6c1c637eac3547901415c363fc
yvbbrjdr <xyyqzy@hotmail.com>
2025-05-10 03:34:47 -0700

    Refactor GET route handler in route.ts
    - Simplified the GET function by removing unnecessary parameters and renaming the request variable to req for consistency.

--- app/[slug]/route.ts
+++ app/[slug]/route.ts
@@ -1,8 +1,5 @@
 import { NextResponse } from "next/server";
 
-export async function GET(
-  request: Request,
-  { params }: { params: { slug: string } }
-) {
-  return NextResponse.redirect(new URL("/", request.url), 302);
+export async function GET(req: Request) {
+  return NextResponse.redirect(new URL("/", req.url), 302);
 }
